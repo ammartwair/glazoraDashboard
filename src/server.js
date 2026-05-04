dd503d181e439582aa33cea7a28061b2e1bd66e5
Ammar Twair <unshakeable1910@gmail.com>
2026-05-04 22:04:12 +0400

    updating origins

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   // Add your production domain here
-  'https://glazora.netlify.app',
+  'https://glazoradashboard.netlify.app',
   'https://glazora.ae',
   'https://www.glazora.ae'
 ].filter(Boolean);
